b995744107447a15d801d458f91bb11c0f7c5237
Akshat Biniwale <akshatbiniwale@gmail.com>
2023-06-25 09:46:14 +0530

    Added refs

--- src/components/UserModal/UserModal.jsx
+++ src/components/UserModal/UserModal.jsx
@@ -1,4 +1,27 @@
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
 import styles from './UserModal.module.css'
+
+const Backdrop = (props)=> {
+    return (<div className={styles.backdrop} onClick={props.onConfirm}/>);
+}
+
+const ModalOverlay = (props)=> {
+    return (
+        <div className={styles['modal-box']}>
+            <div className={styles.heading}>
+                <h1>Invalid Input</h1>
+            </div>
+            <div className={styles['text-box']}>
+                <p>{props.errMsg}</p>
+            </div>
+            <div className={styles['button-box']}>
+                <button onClick={props.onConfirm}>Okay</button>
+            </div>
+        </div>
+    );
+}
 
 const UserModal = (props)=> {
     const closeModalHandler = ()=> {
@@ -6,19 +29,10 @@ const UserModal = (props)=> {
     }
 
     return(
-        <div className={styles.backdrop}>
-            <div className={styles['modal-box']}>
-                <div className={styles.heading}>
-                    <h1>Invalid Input</h1>
-                </div>
-                <div className={styles['text-box']}>
-                    <p>{props.errMsg}</p>
-                </div>
-                <div className={styles['button-box']}>
-                    <button onClick={closeModalHandler}>Okay</button>
-                </div>
-            </div>
-        </div>
+        <Fragment>
+            {ReactDOM.createPortal(<Backdrop onConfirm={closeModalHandler} />, document.getElementById("backdrop-root"))}
+            {ReactDOM.createPortal(<ModalOverlay errMsg={props.errMsg} onConfirm={closeModalHandler} />, document.getElementById("overlay-root"))}
+        </Fragment>
     );
 }
 
